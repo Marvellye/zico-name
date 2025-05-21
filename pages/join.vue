51de0d59d6cd269269f515e76735a9d3fcec42d3
Marvellous Ezekiel <114520580+Marvellye@users.noreply.github.com>
2025-05-22 00:29:55 +0100

    Update join.vue

--- pages/join.vue
+++ pages/join.vue
@@ -1,1 +1,11 @@
+<template>
+  <div class="content">
+    <ThreeScene />
+    <ComingSoon />
+  </div>
+</template>
 
+<script setup>
+import ThreeScene from '~/components/ThreeScene.vue';
+import ComingSoon from '~/components/ComingSoon.vue';
+</script>
